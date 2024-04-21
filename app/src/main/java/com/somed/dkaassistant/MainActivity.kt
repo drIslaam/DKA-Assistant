1938e280c3346d2bfe6a0d6fd2251804bb942156
Islaam Abdurrahim <47725306+drIslaam@users.noreply.github.com>
2024-04-22 02:16:55 +0300

    DKA Assistant Login Screen

--- app/src/main/java/com/somed/dkaassistant/MainActivity.kt
+++ app/src/main/java/com/somed/dkaassistant/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
 
+        // gso
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
